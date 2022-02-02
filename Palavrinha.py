c218dda4d85bdca7ad082f0a2b1b3185421dcd26
Milleny Caroliny <milleny@usp.br>
2022-02-02 12:52:31 -0300

    implementacao do random

--- Palavrinha.py
+++ Palavrinha.py
@@ -1,10 +1,14 @@
 import discord
 import json
+import PalavraAleatoria as gerador
 
 with open("configuracoes.config", "r") as jsonfile:
     configFile = json.load(jsonfile)
 
 client = discord.Client()
+
+#gera palavra aleatória
+palavra = gerador.geraPalavra("palavras.txt")
 
 @client.event
 async def on_ready():
@@ -13,5 +17,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print(message.content)
+    
+    msg = message.content.split(" ", 5)
+    if msg[0].const or msg[1] or msg[2] or msg[3] or msg[4] in palavra:
+        messageSend = await message.channel.send("ta no caminho cara")
 
 client.run(configFile["TOKEN"])
